819b7810a2816a3243cf3275326a6cbf0e1c3e4b
unknown <ricardo_cema@hotmail.com>
2025-12-05 22:45:39 -0500

    commit

--- sierradorada_backend/app/core/config.py
+++ sierradorada_backend/app/core/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 class Settings:
     PROJECT_NAME = "api de personas"
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:7dejulio@localhost:5432/sierradorada")
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres.jouafghxsudpaoivdxaq:7dejulio@aws-1-us-east-2.pooler.supabase.com:6543/postgres")
 settings = Settings()
